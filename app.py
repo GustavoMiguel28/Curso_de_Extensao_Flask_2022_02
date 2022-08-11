24559474e4aff64f006b888d28674b56994e02de
Gustavo Miguel <gustavo_pito@hotmail.com>
2022-08-10 22:04:42 -0300

    Criando tabela

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def ola_mundo():
     #criando uma variavel com o meu nome
     nome = 'Gustavo Miguel'
 
-    return render_template("alo.html", n=nome), 200
+    produtos = [ 
+        {'nome': 'Ração', 'preco': 199.99},
+        {'nome': 'Playstation', 'preco': 1999.99}
+    ]
+
+    return render_template("alo.html", n=nome, aProdutos=produtos), 200
 
 app.run()
